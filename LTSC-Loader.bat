bb77176b0859e5b778f60bea580891eb4e3ce701
riadkabir45 <riadkabir45@gmail.com>
2021-02-10 18:53:44 +0600

    LTSC-loaderV1

---
+++ LTSC-Loader.bat
@@ -0,0 +1,9 @@
+C:\Windows\System32\spp\tokens\skus
+
+cscript.exe %windir%\system32\slmgr.vbs /rilc
+cscript.exe %windir%\system32\slmgr.vbs /upk >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /ckms >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /cpky >nul 2>&1
+cscript.exe %windir%\system32\slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+slmgr.vbs /skms kms.teevee.asia 
+slmgr.vbs /ato
